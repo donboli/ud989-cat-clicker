5502ff69dd8dee6fa5745c5a57933f781fb7462d
Juan Becker <wolli.becker@gmail.com>
2017-01-21 18:53:35 +0100

    add admin view

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ $(function() {
 
 	var model = {
 		init: function() {
+			this.adminVisible = false;
 			this.cats = {};
 
 			for (var i = 1; i < 6; i++) {
 				this.cats['cat' + i] = {
-					src: 'cat_picture' + i + '.jpg',
-					counter: 0
+					name: 'Cat ' + i,
+					imgURL: 'cat_picture' + i + '.jpg',
+					clicks: 0
 				};
 			}
 		},
@@ -16,12 +18,30 @@ $(function() {
 			return this.cats;
 		},
 
-		get: function(id) {
-			return this.cats[id];
+		getCurrentCat: function() {
+			return this.currentCat;
 		},
 
-		incrementCounter(id) {
-			this.cats[id].counter++;
+		setCurrentCat: function(id) {
+			this.currentCat = this.cats[id];
+		},
+
+		updateCurrentCat: function(name, imgURL, clicks) {
+			this.currentCat.name = name;
+			this.currentCat.imgURL = imgURL;
+			this.currentCat.clicks = clicks;
+		},
+
+		incrementClicks: function() {
+			this.currentCat.clicks++;
+		},
+
+		showAdmin: function() {
+			this.adminVisible = true;
+		},
+
+		hideAdmin: function() {
+			this.adminVisible = false;
 		}
 	};
 
@@ -29,36 +49,65 @@ $(function() {
 		init: function() {
 			model.init();
 			listView.init();
-			listView.render();
 			catView.init();
+			adminView.init();
 		},
 
 		getCats: function() {
 			return model.getAll();
 		},
 
-		getCat: function(id) {
-			return model.get(id);
+		getCurrentCat: function() {
+			return model.getCurrentCat();
 		},
 
-		incrementCounter: function(id) {
-			model.incrementCounter(id);
+		incrementClicks: function() {
+			model.incrementClicks();
+			octopus.hideAdmin();
+			catView.render();
+		},
+
+		setCurrentCat: function(id) {
+			model.setCurrentCat(id);
+		},
+
+		showAdmin: function() {
+			model.showAdmin();
+			adminView.render();
+		},
+
+		hideAdmin: function() {
+			model.hideAdmin();
+			adminView.render();
+		},
+
+		getAdminVisible: function() {
+			return model.adminVisible;
+		},
+
+		updateCurrentCat: function(name, imgURL, clicks) {
+			model.updateCurrentCat(name, imgURL, clicks);
+			octopus.hideAdmin();
+			listView.render();
+			catView.render();
 		}
 	};
 
 	var listView = {
 		init: function() {
-			this.buttons = [];
 			this.$catList = $('#catList');
+			this.render();
 		},
 
 		render: function() {
+			this.buttons = [];
 			var cats =  octopus.getCats();
+			this.$catList.empty();
 
 			// set button list
 			for (var cat in cats) {
 				if (cats.hasOwnProperty(cat)) {
-					this.buttons.push("<button data-id='" + cat + "'>" + cat + "</button>");
+					this.buttons.push("<button data-id='" + cat + "'>" + cats[cat].name + "</button>");
 				}
 			}
 
@@ -66,7 +115,9 @@ $(function() {
 
 			// add event listener for cat buttons
 			this.$catList.on('click', 'button', function() {
-				catView.render($(this).data('id'));
+				octopus.hideAdmin();
+				octopus.setCurrentCat($(this).data('id'));
+				catView.render();
 			});
 		}
 	};
@@ -78,19 +129,67 @@ $(function() {
 			// add event listener for cat images
 			this.$catView.on('click', 'img', function() {
 				var id = $(this).parent('.cat').data('id');
-				octopus.incrementCounter(id);
-				catView.render(id);
+				octopus.incrementClicks();
 			});
 		},
 
 		render: function(id) {
-			cat = octopus.getCat(id);
+			cat = octopus.getCurrentCat();
 
 			this.$catView.empty();
 			this.$catView.append("<div class='cat' data-id='" + id +
-				"'><span class='counter'>" + cat.counter +
-				"</span> clicks<br><img class='clicker' src='" + cat.src +
+				"'><span class='clicks'>" + cat.clicks +
+				"</span> clicks<br><img class='clicker' src='" + cat.imgURL +
 				"'></div>");
+		}
+	};
+
+	var adminView = {
+		init: function() {
+			this.$adminForm = $('#adminForm');
+
+			// buttons
+			this.$adminButton = $('#adminButton');
+			this.$cancelButton = $('#cancelButton');
+
+			// form fields
+			this.$name = $('#name');
+			this.$imgURL = $('#imgURL');
+			this.$clicks = $('#clicks');
+
+			// event handlers
+			this.$adminForm.on('submit', function(e) {
+				e.preventDefault();
+				var name = adminView.$name.val();
+				var imgURL = adminView.$imgURL.val();
+				var clicks = adminView.$clicks.val();
+
+				octopus.updateCurrentCat(name, imgURL, clicks);
+			});
+
+			this.$cancelButton.on('click', function() {
+				octopus.hideAdmin();
+			});
+
+			this.$adminButton.on('click', function() {
+				octopus.showAdmin();
+			});
+
+			this.render();
+		},
+
+		render: function() {
+			if(octopus.getAdminVisible()) {
+				cat = octopus.getCurrentCat();
+
+				this.$name.val(cat.name);
+				this.$imgURL.val(cat.imgURL);
+				this.$clicks.val(cat.clicks);
+
+				this.$adminForm.show();
+			} else {
+				this.$adminForm.hide();
+			}
 		}
 	};
 
